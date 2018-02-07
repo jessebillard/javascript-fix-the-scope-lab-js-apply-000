340dc31ca3a323a13cca117f12658729f6144023
Jesse Billard <jessebillard@mac.com>
2018-02-07 15:59:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
   return animal[1]
 }
 
 function add2(n) {
   const two = 2 
   return n + two
-  // Feel free to move things around!
 }
 
 var funkyFunction = function() {
